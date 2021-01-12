20d72faf6281b81714c3266538b7a1f779a54d97
Yanay Goor <yanay.goor@gmail.com>
2021-01-12 13:25:19 -0800

    WIP Add pico interface file

--- usermode/src/main.c
+++ usermode/src/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <poll.h>
+#include <pico.h>
 
 union pipefds {
     struct {
@@ -11,12 +14,36 @@ union pipefds {
     int fds[2];
 };
 
-int main(void) {
+int main(int argc, char *argv[]) {
     union pipefds to_child;
     union pipefds from_child;
     char buff[1024];
     size_t size;
     struct pollfd pfd[2]; 
+    struct pico_socket *sock;
+
+    int port;
+    int dst_port;
+
+    if (argc != 4) {
+        printf("Wrong amount of arguments.\n");
+        printf("Usage: %s [PORT] [DST_HOST] [DST_PORT]\n", argv[0]);
+        return 2;
+    }
+
+    port = atoi(argv[1]);
+    if (port < 1 || port >> 16) {
+        printf("Invalid port\n");
+        return 3;
+    }
+
+    dst_port = atoi(argv[3]);
+    if (dst_port < 1 || dst_port >> 16) {
+        printf("Invalid dst port\n");
+        return 4;
+    }
+
+    sock = create_socket((uint16_t)port, argv[1], (uint16_t)dst_port);
 
     if (pipe(to_child.fds) == -1) return 1;
     if (pipe(from_child.fds) == -1) return 1;
